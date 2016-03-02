351b9e74d8c55bc3572d4b7cce7a302d148309d7
Mark Heckler <mark.heckler@gmail.com>
2016-03-02 12:44:36 -0600

    Testing 1, 2, 3...

--- src/main/java/org/thehecklers/EdgeServiceApplication.java
+++ src/main/java/org/thehecklers/EdgeServiceApplication.java
@@ -9,30 +9,74 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableOAuth2Sso
-public class EdgeServiceApplication {
+public class EdgeServiceApplication extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/index.html", "/home.html", "/")
+                .permitAll().anyRequest().authenticated(); //.and().csrf()
+//                .csrfTokenRepository(csrfTokenRepository()).and()
+//                .addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class);
+    }
 
-	public static void main(String[] args) {
+//    private CsrfTokenRepository csrfTokenRepository() {
+//        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+//        repository.setHeaderName("X-XSRF-TOKEN");
+//        return repository;
+//    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(EdgeServiceApplication.class, args);
 	}
 }
 
+//// Test with, without - MAH
+//class CsrfHeaderFilter extends OncePerRequestFilter {
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request,
+//                                    HttpServletResponse response, FilterChain filterChain)
+//            throws ServletException, IOException {
+//        CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class
+//                .getName());
+//        if (csrf != null) {
+//            Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+//            String token = csrf.getToken();
+//            if (cookie==null || token!=null && !token.equals(cookie.getValue())) {
+//                cookie = new Cookie("XSRF-TOKEN", token);
+//                cookie.setPath("/");
+//                response.addCookie(cookie);
+//            }
+//        }
+//        filterChain.doFilter(request, response);
+//    }
+//}
+
 @RestController
 class QuoteController {
-    @Autowired
-    RestTemplate restTemplate;
+//    @Autowired
+//    RestTemplate restTemplate;
 
     @RequestMapping("/quotorama")
     public Quote getRandomQuote() {
-//        RestTemplate restTemplate = new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject("http://QUOTE-SERVICE/random", Quote.class);
 //        return new Quote(1L, "Test quote.", "Testquoticus");
     }
